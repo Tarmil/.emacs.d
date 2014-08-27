3a4b561dfe788b3fe0a9f9399146408b7035d935
Loic Denuziere <loic@denuziere.net>
2014-08-27 22:41:44 +0200

    Fix custom-set-faces

--- init.el
+++ init.el
@@ -8,4 +8,10 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :height 90 :family "Consolas"))))
  '(fsharp-error-face ((t (:inherit error))) t)
- '(highlight ((t (:background "#333" :foreground "white")))))
+ '(highlight ((t (:background "#333" :foreground "white"))))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
