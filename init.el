6f286d1f3cd6a459e126ea81339ea06c8eef9eb4
Loic Denuziere <loic@denuziere.net>
2014-09-27 17:45:18 +0200

    helm directory background color

--- init.el
+++ init.el
@@ -8,7 +8,8 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :height 90 :family "Consolas"))))
  '(fsharp-error-face ((t (:inherit error))) t)
- '(highlight ((t (:background "#333" :foreground "white"))))))
+ '(highlight ((t (:background "#333" :foreground "white"))))
+ '(helm-ff-directory ((t (:background nil)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
