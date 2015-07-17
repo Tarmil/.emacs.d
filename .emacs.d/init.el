2fae563adc03bfb76a4e5a1a14201cac8025f8e5
Loic Denuziere <loic@denuziere.net>
2015-07-17 19:41:05 +0200

    Deactivate indent-tabs-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,8 @@
 (require 'org)
 (org-babel-load-file "~/.emacs.d/init-el.org")
 
+(setq magit-last-seen-setup-instructions "1.4.0")
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,6 +18,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(delete-selection-mode t)
+ '(indent-tabs-mode nil)
  '(initial-scratch-message nil)
  '(org-CUA-compatible nil)
  '(org-replace-disputed-keys nil)
