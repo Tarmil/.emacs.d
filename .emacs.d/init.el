adabc3291f3d0e2ebf1fba66f728802d67f220f1
Loic Denuziere <loic@denuziere.net>
2015-07-22 08:37:55 +0200

    Emacs font: Bitstream Vera Sans Mono

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :height 90 :family "Consolas"))))
+ '(default ((t (:inherit nil :height 80 :family "Bitstream Vera Sans Mono"))))
  '(fsharp-error-face ((t (:inherit error))))
  '(helm-ff-directory ((t (:background nil))))
  '(highlight ((t (:background "#333" :foreground "white")))))
