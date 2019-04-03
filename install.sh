ebb0cd5186fae2732adf193fddb3e6efb4139fc1
Loic Denuziere <ldenuziere@siriona.com>
2019-04-03 16:55:49 +0200

    Move to spacemacs

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@ link() {
     ln -s "$PWD/$1" "$HOME/$2" 2> /dev/null && echo "Linked: $1" || echo "Exists: $1"
 }
 
-for x in .emacs.d .xmobarrc .xmonad .Xdefaults
+for x in .spacemacs .xmobarrc .xmonad .Xdefaults
 do
     link "$x"
 done
+
+if ! [ -d "$HOME/.emacs" ]; then
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
 
 link polybar/ .config/
